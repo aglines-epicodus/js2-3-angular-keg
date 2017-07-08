1be4d3eb6c41a2d4589ef4860e2475ebf3ff087e
Andrew Glines <aglines@gmail.com>
2017-07-07 23:44:36 -0700

    add functionality to split code into parent and child components, and pass data down, and method actions up

---
+++ app/animal-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Animal } from './animal.model';
+
+@Component({
+  selector: 'animal-list',
+  template: `
+  <ul>
+    <li *ngFor="let currentAnimal of childAnimalList">{{currentAnimal.name}}  <button (click)="editButtonHasBeenClicked(currentAnimal)">Edit</button></li>
+  </ul>
+  `
+})
+
+export class AnimalListComponent {
+  @Input() childAnimalList: Animal[];
+  @Output() clickSender = new EventEmitter();
+
+  editButtonHasBeenClicked(animalToEdit: Animal) {
+    this.clickSender.emit(animalToEdit);
+  }
+
+  // animals: Animal[] = [
+  //   // list of animals used to be here
+  // ];
+}
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Animal } from './animal.model';
 
 @Component({
   selector: 'app-root',
@@ -6,10 +7,8 @@ import { Component } from '@angular/core';
   <div class="container">
     <h1>Zoo-Do List</h1>
     <h3>Today is {{month}}/{{day}}</h3>
-    <ul>
-      <li *ngFor="let currentAnimal of animals">{{currentAnimal.name}}  <button (click)="editAnimal(currentAnimal)">Edit</button></li>
-    </ul>
     <hr>
+    <animal-list [childAnimalList]="masterAnimalList" (clickSender)="editAnimal($event)"></animal-list>
     </div>
     <div class="well">
     <div *ngIf="selectedAnimal">
@@ -34,15 +33,13 @@ export class AppComponent {
   currentTime = new Date();
   month: number = this.currentTime.getMonth() + 1;
   day: number = this.currentTime.getDate();
+  selectedAnimal = null;
 
-  animals: Animal[] = [
+  masterAnimalList: Animal[] = [
     new Animal("Arctic Fox", "Moon", 2, "Carnivore", "Northern Trail", 5,"Female", "Cool shade", "Loud noises"),
     new Animal("Ocelot","Prince",4,"Carnivore","Tropical Rain Forest Building",6,"Male","Laying in the sunshine","Toys that are not rope-based"),
     new Animal("Northwest Black Tailed Deer","Tinkerbell",8,"Herbivore","Northern Trail",2,"Female","Delicate roots and leaves","Loud Noises")
   ];
-
-  // selectedAnimal: Animal = this.animals[0];
-  selectedAnimal = null;
 
   editAnimal(clickedAnimal) {
     this.selectedAnimal = clickedAnimal;
@@ -51,8 +48,4 @@ export class AppComponent {
     this.selectedAnimal = null;
   }
 
-}
-
-export class Animal {
-  constructor(public species: string, public name: string, public age: number, public diet: string, public location: string, public caretakers: number, public sex: string, public likes: string, public dislikes: string) {}
 }
